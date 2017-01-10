a0705bcc88b7e58bd7ad7e42c0821fa460a18d98
Andrew Keats <andrew.keats@digital.hmrc.gov.uk>
2017-01-10 12:04:49 +0000

    successfully remapping the module paths for actions

--- src/cli/JSONPropertyResolver.ts
+++ src/cli/JSONPropertyResolver.ts
@@ -19,25 +19,35 @@ module jm.cli {
     const PATH_EMPTY_JSON: string = process.cwd() + '/resources/empty.json';
     const SPECIAL_CASE_KEYS: string[] = ['actions', 'validator'];
 
-    function reMapModulePaths(aModuleMap:{ [pointer:string]: string}, aJSObject: Object): Object {
-        let pointers: string[] = Object.keys(aModuleMap);
+    function reMapModulePaths(aProcessedJson:JsonRefs.ResolvedRefsResults): Object {
+        let resovedJson = aProcessedJson.resolved,
+            refs: { [JSONPointer: string]: JsonRefs.ResolvedRefDetails } = aProcessedJson.refs,
+            pointers: string[] = Object.keys(refs).filter((aPointer:string) => {
+                return !!(<{ _$ref: string; }>refs[aPointer].def)._$ref;
+            });
 
         pointers.forEach((aKey: string) => {
             let pathToProp: string[] = JSONRefs.pathFromPtr(aKey),
                 lastObj: ObjOrArr,
                 pathLength:number = pathToProp.length,
                 pathIndex: number = 0,
-                nextObj:ObjOrArr = aJSObject;
+                nextObj:ObjOrArr = resovedJson,
+                nextKey: string;
 
-            while((pathIndex < pathIndex)){
+            while((pathIndex < pathLength)){
                 lastObj = nextObj;
-                nextObj = lastObj[pathToProp[pathIndex]];
+                nextKey = pathToProp[pathIndex];
+                nextObj = lastObj[nextKey];
                 ++pathIndex;
             }
 
+            //Now we've got hold of the right object to rectify,
+            //we can add the new ref object that has the absolute path to the module
+            lastObj[nextKey] = (<{ _$ref: string; }>refs[aKey].def)._$ref;
+
         });
 
-        return aJSObject;
+        return resovedJson;
     }
 
     function getLastKeyInPathIfSpecial(aFragmentedPath:string[]): string {
@@ -53,49 +63,23 @@ module jm.cli {
         function processor(obj: { $ref: string }, refPath: string[]) {
             let objToReturn:{ $ref: string } | { _$ref: string },
                 lastPathKey:string = getLastKeyInPathIfSpecial(refPath);
-            console.log('refPreProcessor');
-            console.log(path.resolve(this.relativeBase, obj.$ref));
+            //console.log('refPreProcessor');
 
             if (lastPathKey) {
-                console.log('found: ', lastPathKey);
-                moduleTable[JSONRefs.pathToPtr(refPath)] = path.resolve(this.relativeBase, obj.$ref);
-                //objToReturn = require(path.resolve(this.relativeBase, obj.$ref));
-                //obj.$ref = path.resolve(this.relativeBase, obj.$ref);
-                //obj._$ref = path.resolve(this.relativeBase, obj.$ref);
-                //obj.$ref = undefined;
+                //console.log('found: ', lastPathKey);
                 objToReturn = { _$ref: path.resolve(this.relativeBase, obj.$ref), $ref: path.relative(this.relativeBase, PATH_EMPTY_JSON) };
-                //objToReturn = obj;
             } else {
                 objToReturn = obj;
             }
 
             return objToReturn;
         }
-        function postProcessor(obj: JsonRefs.UnresolvedRefDetails, refPath: string[]) {
-            var objToReturn;
-            console.log('postProcessor');
 
-            if ((<any>obj.def)._$ref) {
-                console.log('we found a cheeky non json function');
-                //obj.uri = undefined;
-                //objToReturn = obj.def;
-                objToReturn = obj;
-            }
-            else {
-                objToReturn = obj;
-            }
-            return objToReturn;
-        }
-
-        let moduleTable = {},
-            errorHandler = ErrorHandler.setupPromiseErrorHandler(JSON_RESOLUTION_FAILED, aJSObject),
+        let errorHandler = ErrorHandler.setupPromiseErrorHandler(JSON_RESOLUTION_FAILED, aJSObject),
             config:JsonRefs.JsonRefsOptions = {
                 relativeBase: aBasePath,
                 includeInvalid: true,
-                //filter: refFilter
-                //filter: excludeSpecialPaths,
-                refPreProcessor: processor,//,
-                refPostProcessor: postProcessor
+                refPreProcessor: processor
             },
             resolution: Promise<JsonRefs.ResolvedRefsResults> = JSONRefs.resolveRefs(aJSObject, config),
             resolveJson: (aJsonRef: JsonRefs.ResolvedRefsResults) => Promise<Object> = (aJsonRef: JsonRefs.ResolvedRefsResults) => {
@@ -111,7 +95,7 @@ module jm.cli {
                 if (errors.length) {
                     return errorHandler(new Error(errors[0]));
                 } else {
-                    return Promise.resolve(reMapModulePaths(moduleTable, aJsonRef.resolved));
+                    return Promise.resolve(reMapModulePaths(aJsonRef));
                 }
 
             };
