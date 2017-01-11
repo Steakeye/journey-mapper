e8f58b2cac93c1ea91781f156e6917305844853b
Andrew Keats <andrew.keats@digital.hmrc.gov.uk>
2017-01-11 19:09:21 +0000

    wip: working on interacting with the view

--- src/core/Step.ts
+++ src/core/Step.ts
@@ -2,6 +2,8 @@ import { LinkItem } from './LinkItem'
 import {unescape} from "querystring";
 
 module jm.core {
+    type DeferredQuery = SQuery| JQuery;
+
     type ScreenshotCue_onLoad = "on_load";
     type ScreenshotCue_onInteract = "on_interaction";
     type ScreenshotCue = ScreenshotCue_onLoad | ScreenshotCue_onInteract;
@@ -40,13 +42,25 @@ module jm.core {
 
             //Check for screenshot requirement
             if ((<ModernArray<ScreenshotCue>>this.screenShotCues).includes(onLoadCue)) {
-                this.nav.takeScreenshot().then(this.makeScreenshotSaver(onLoadCue));
+                this.nav.takeScreenshot().then(this.makeScreenShotSaver(onLoadCue));
             }
 
             this.isExpectedState(aCurrentState).then((aExpected: boolean) => {
-                this.interact(aCurrentState);
+                this.setValidation(true, true);
+
+                this.interact(aCurrentState).then((aCurrentState: DeferredQuery) => {
+                    //TODO
+                    console.log('interact.then()');
+                    this.handleCompletedInteraction()
+                },
+                (aErr: any) => {
+                    //TODO
+                    console.log('interact.refect()');
+                    this.handleFailedInteraction();
+                });
             },
             (aErr: string) => {
+                this.setValidation(true, false);
                 this.errorHandler(Step.MSG_INCORRECT_STATE);
             });
         }
@@ -82,20 +96,39 @@ module jm.core {
             });
         }
 
-        private interact(aState: SQuery| JQuery) : void {
-            this.interactor(this, aState, this.errorHandler);
+        private handleCompletedInteraction(): void {
+            //TODO!
+        }
+        private handleFailedInteraction(): void {
+            //TODO
         }
 
-        private makeScreenshotSaver(aCueName: ScreenshotCue): (aData: string) => void {
+        private interact(aState: SQuery| JQuery) : Promise<DeferredQuery> {
+            return this.interactor(this, aState, this.errorHandler);
+        }
+
+        private makeScreenShotSaver(aCueName: ScreenshotCue): (aData: string) => void {
             return (aData: string) => {
                 this.screenShots[aCueName] = aData;
             }
         }
 
+        private setValidation(aValidationAttempt:boolean, aPassed: boolean): void {
+            this.hasBeenValidated = aValidationAttempt;
+            this.isValid = aPassed;
+        }
+
         private screenShotCues: ScreenshotCue[];
+
         private screenShots: ScreenShotMap = {};
-        private interactor: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => void;
+
+        private interactor: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => Promise<DeferredQuery>;
+
         private validator?: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => boolean | Promise<boolean>;
+
+        private hasBeenValidated: boolean = false;
+
+        private isValid: boolean;
     }
 }
 export = jm.core;
--- test/actions/feeling_lucky.js
+++ test/actions/feeling_lucky.js
@@ -1,5 +1,8 @@
-function feelingLucky(aStep, aNav, aErrorFunc) {
+function feelingLucky(aStep, aQueryObj, aErrorFunc) {
     console.log('We got to eh feeling luck func!');
+    return aQueryObj.find('.jsb')
+        .filter('input[name="btnI"]')
+        .click()
 }
 
 module.exports = feelingLucky;
