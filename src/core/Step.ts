b75c1772eabb976471e9945386e7c711cc2fd549
Andrew Keats <andrew.keats@digital.hmrc.gov.uk>
2017-01-11 16:10:24 +0000

    wip: working on validator proof of concept

--- src/core/Step.ts
+++ src/core/Step.ts
@@ -18,11 +18,17 @@ module jm.core {
 
         public begin(aCurrentState: SQuery| JQuery) : void {
             //TODO
-            if (this.isExpectedState(aCurrentState)) {
+            /*if (this.isExpectedState(aCurrentState)) {
                 this.interact(aCurrentState)
             } else {
                 this.errorHandler(Step.MSG_INCORRECT_STATE);
-            }
+            }*/
+            this.isExpectedState(aCurrentState).then((aExpected: boolean) => {
+                this.interact(aCurrentState);
+            },
+            (aErr: string) => {
+                this.errorHandler(aErr);
+            });
         }
 
         private build(aStep: StepConfig): void {
@@ -37,9 +43,20 @@ module jm.core {
             this.validator = path ? require(path) : (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aNavigator: NavigatorAdaptor) => true;
         }
 
-        private isExpectedState(aCurrentState: SQuery| JQuery) : boolean {
-            //TODO
-            return this.validator(this, aCurrentState, this.errorHandler);
+        private isExpectedState(aCurrentState: SQuery| JQuery) : Promise<boolean> {
+            return new Promise<boolean>((resolve : (value?: boolean) => void, reject: (error?: any) => void) => {
+                let res = this.validator(this, aCurrentState, this.errorHandler);
+
+                if (res !== undefined && !(res instanceof Promise)) {
+                    if (res) {
+                        resolve(res);
+                    } else {
+                        reject(Step.MSG_INCORRECT_STATE);
+                    }
+                } else {
+                    (<Promise<boolean>>res).then(resolve, reject);
+                }
+            });
         }
 
         private interact(aState: SQuery| JQuery) : void {
@@ -48,7 +65,7 @@ module jm.core {
         }
 
         private interactor: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => void;
-        private validator?: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => boolean;
+        private validator?: (aCurrentStep: Step, aCurrentState: SQuery| JQuery, aErrHandler: BasicErrorHandler) => boolean | Promise<boolean>;
     }
 }
 export = jm.core;
--- test/validators/is_google_home.js
+++ test/validators/is_google_home.js
@@ -1,5 +1,29 @@
-function isGoogleHome(aStep, aNav, aErrorFunc) {
-    console.log('We got to gogle home validator!');
+function isGoogleHome(aStep, aQueryObj, aErrorFunc) {
+    var urlToMatch = "http://google.com"; //https://www.google.co.uk/
+
+    console.log('We got to google home validator!');
+
+    function makeRejection(aErrorCB) {
+        return function rejectUrl(aError) {
+            aErrorCB(false)
+        }
+    }
+
+    function makeResolution(aResolveCB, aRejectCB){
+        return function testUrl(aUrl) {
+            if (aUrl === urlToMatch) {
+                aResolveCB(true);
+            } else {
+                aRejectCB(false);
+            }
+        }
+    }
+
+    function getUrlAndResolve(aOnResolve, aOnReject) {
+        aStep.nav.getCurrentUrl().then(makeResolution(aOnResolve, aOnReject), makeRejection(aOnReject));
+    }
+
+    return new Promise(getUrlAndResolve);
 }
 
 module.exports = isGoogleHome;
